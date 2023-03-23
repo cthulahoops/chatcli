2647e138d8ba2cb418e72933e29bb2b7e0704c01
Adam Kelly <adam@cthulahoops.org>
2023-03-23 11:37:33 +0000

    Update the tests for chatcli.py.
    All tests that use the "prompt" function now use the "input" parameter in the "invoke" function instead. Also, the "mocker" parameter is not needed in some tests anymore and was removed.

--- test_chatcli.py
+++ test_chatcli.py
@@ -21,22 +21,20 @@ def fake_assistant(mocker):
     mocker.patch("chatcli.openai.ChatCompletion.create", advanced_ai)
 
 
-def test_chat_code(mocker):
-    mocker.patch("chatcli.prompt", return_value="Say hello in python")
+def test_chat_code():
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(
-            cli, ["chat", "--quick", "-p", "code"], catch_exceptions=False
+            cli, ["chat", "--quick", "-p", "code"], input="Say hello in python", catch_exceptions=False
         )
         assert result.exit_code == 0
         assert "SAY HELLO IN PYTHON" in result.output
 
 
-def test_show_short(mocker):
+def test_show_short():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        mocker.patch("chatcli.prompt", return_value="What is your name?")
-        result = runner.invoke(cli, ["chat", "-q"], catch_exceptions=False)
+        result = runner.invoke(cli, ["chat", "-q"], input="What is your name?", catch_exceptions=False)
         result = runner.invoke(cli, ["show", "-s"], catch_exceptions=False)
         assert result.exit_code == 0
         assert "What is your name?" not in result.output
@@ -46,8 +44,7 @@ def test_show_short(mocker):
 def test_show_long(mocker):
     runner = CliRunner()
     with runner.isolated_filesystem():
-        mocker.patch("chatcli.prompt", return_value="What is your name?")
-        result = runner.invoke(cli, ["chat", "-q"], catch_exceptions=False)
+        result = runner.invoke(cli, ["chat", "-q"], input="What is your name?", catch_exceptions=False)
         result = runner.invoke(cli, ["show", "-l"], catch_exceptions=False)
         assert result.exit_code == 0
         assert "expert linux user" in result.output
@@ -58,53 +55,46 @@ def test_show_long(mocker):
 def test_log(mocker):
     runner = CliRunner()
     with runner.isolated_filesystem():
-        mocker.patch("chatcli.prompt", return_value="What is your name?")
         result = runner.invoke(
-            cli, ["chat", "--quick", "-p", "concise"], catch_exceptions=False
+            cli, ["chat", "--quick", "-p", "concise"], input="What is your name?", catch_exceptions=False
         )
-        mocker.patch("chatcli.prompt", return_value="What is your quest?")
         result = runner.invoke(
-            cli, ["chat", "--quick", "-c", "-p", "italiano"], catch_exceptions=False
+            cli, ["chat", "--quick", "-c", "-p", "italiano"], input="What is your quest?", catch_exceptions=False
         )
         result = runner.invoke(cli, ["log"], catch_exceptions=False)
         assert result.exit_code == 0
         assert "2: What is your name?" in result.output
         assert "1: What is your quest?" in result.output
 
-def test_chat_log_search(mocker):
+def test_chat_log_search():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        mocker.patch("chatcli.prompt", return_value="What is your name?")
         result = runner.invoke(
-            cli, ["chat", "--quick", "-p", "concise"], catch_exceptions=False
+            cli, ["chat", "--quick", "-p", "concise"], input="What is your name?", catch_exceptions=False
         )
-        mocker.patch("chatcli.prompt", return_value="What is your quest?")
         result = runner.invoke(
-            cli, ["chat", "--quick", "-c", "-p", "italiano"], catch_exceptions=False
+            cli, ["chat", "--quick", "-c", "-p", "italiano"], input="What is your quest?", catch_exceptions=False
         )
         result = runner.invoke(cli, ["log", "-s", "name"], catch_exceptions=False)
         assert result.exit_code == 0
         assert "2: What is your name?" in result.output
         assert "1: What is your quest?" not in result.output
 
-def test_usage(mocker):
-    mocker.patch("chatcli.prompt", return_value="What is your name?")
-
+def test_usage():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(cli, ["chat", "--quick"], catch_exceptions=False)
-        result = runner.invoke(cli, ["chat", "--quick"], catch_exceptions=False)
+        result = runner.invoke(cli, ["chat"], input="What is your name?", catch_exceptions=False)
+        result = runner.invoke(cli, ["chat"], input="What is your name?", catch_exceptions=False)
         result = runner.invoke(cli, ["usage"], catch_exceptions=False)
         assert result.exit_code == 0
         assert "Tokens: 82" in result.output
         assert "Cost: $0.00" in result.output
 
 
-def test_chat_retry(mocker):
+def test_chat_retry():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        with patch("chatcli.prompt", return_value="What is your name?"):
-            result = runner.invoke(cli, ["chat", "--quick"], catch_exceptions=False)
+        result = runner.invoke(cli, ["chat"], input="What is your name?", catch_exceptions=False)
         result = runner.invoke(
             cli, ["chat", "--quick", "--retry"], catch_exceptions=False
         )
