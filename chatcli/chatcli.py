556f7e38155e3aa0eb765c4a8f4a550cbbd99f2f
Adam Kelly <adam@cthulahoops.org>
2023-03-25 08:11:00 +0000

    Reformat with black.

--- chatcli/chatcli.py
+++ chatcli/chatcli.py
@@ -17,48 +17,70 @@ INITIAL_PERSONALITIES = {
     "concise": "You are a helpful, expert linux user and programmer. You give concise answers, providing code where possible.",
     "code": "You only answer questions with a single example code block only, and no other explanations.",
     "commit": """You generate commit messages from diffs. Every line of commit message should be less than eighty characters.
-You never output anything that does not belong in the commit message."""
+You never output anything that does not belong in the commit message.""",
 }
 
-@click.group(cls=DefaultGroup, default='chat', default_if_no_args=True)
+
+@click.group(cls=DefaultGroup, default="chat", default_if_no_args=True)
 def cli():
     pass
 
+
 def cli_search_options(command):
-    @click.option('-n', '--offset', type=int, help="Message offset")
-    @click.option('-s', '--search', help="Select by search term")
-    @click.option('-t', '--tag', help="Select by tag")
+    @click.option("-n", "--offset", type=int, help="Message offset")
+    @click.option("-s", "--search", help="Select by search term")
+    @click.option("-t", "--tag", help="Select by tag")
     @functools.wraps(command)
     def wrapper(*args, offset=None, search=None, tag=None, **kwargs):
-        return command(*args, search_options={
-            'offset': offset,
-            'search': search,
-            'tag': tag}, **kwargs)
+        return command(
+            *args,
+            search_options={"offset": offset, "search": search, "tag": tag},
+            **kwargs,
+        )
+
     return wrapper
 
+
 @cli.command(help="Ask a question of ChatGPT.")
-@click.option('-q', '--quick', is_flag=True, help="Just handle a one single-line question.")
-@click.option('-c', '--continue_conversation', '--continue', is_flag=True, help="Continue previous conversation.")
-@click.option('-p', '--personality', default='concise')
-@click.option('-f', '--file', type=click.Path(exists=True), multiple=True, help="Add a file to the conversation for context.")
-@click.option('-r', '--retry', is_flag=True, help="Retry previous question")
-@click.option('--stream/--sync', default=True, help="Stream or sync mode.")
+@click.option("-q", "--quick", is_flag=True, help="Just handle a one single-line question.")
+@click.option(
+    "-c",
+    "--continue_conversation",
+    "--continue",
+    is_flag=True,
+    help="Continue previous conversation.",
+)
+@click.option("-p", "--personality", default="concise")
+@click.option(
+    "-f",
+    "--file",
+    type=click.Path(exists=True),
+    multiple=True,
+    help="Add a file to the conversation for context.",
+)
+@click.option("-r", "--retry", is_flag=True, help="Retry previous question")
+@click.option("--stream/--sync", default=True, help="Stream or sync mode.")
 @cli_search_options
 def chat(quick, continue_conversation, personality, file, retry, stream, search_options):
-    if (continue_conversation or retry) and not search_options['offset']:
+    if (continue_conversation or retry) and not search_options["offset"]:
         search_options["offset"] = 1
-    elif personality and not search_options['tag'] and not search_options['search'] and not search_options['offset']:
+    elif personality and not search_options["tag"] and not search_options["search"] and not search_options["offset"]:
         search_options["tag"] = "^" + personality
 
     exchange = get_logged_exchange(**search_options)
-    request_messages = exchange['messages']
+    request_messages = exchange["messages"]
 
     for filename in file:
         with open(filename, encoding="utf-8") as fh:
             file_contents = fh.read()
-        request_messages.append({"role": "user", "content": f"The file {file} contains:\n```\n{file_contents}```"})
+        request_messages.append(
+            {
+                "role": "user",
+                "content": f"The file {file} contains:\n```\n{file_contents}```",
+            }
+        )
 
-    tags = exchange.get('tags', [])
+    tags = exchange.get("tags", [])
     if tags and not tags[-1].startswith("^"):
         tags_to_apply = [tags[-1]]
     else:
@@ -70,19 +92,26 @@ def chat(quick, continue_conversation, personality, file, retry, stream, search_
             request_messages.append(response)
             conversation(request_messages, stream=stream, tags=tags_to_apply)
     elif quick or not os.isatty(0):
-        conversation(request_messages, stream=stream, tags=tags_to_apply, quick=True, multiline=False)
+        conversation(
+            request_messages,
+            stream=stream,
+            tags=tags_to_apply,
+            quick=True,
+            multiline=False,
+        )
     else:
         conversation(request_messages, stream=stream, tags=tags_to_apply)
 
+
 @cli.command(help="Add a message to a new or existing conversation.")
-@click.option('--multiline/--singleline', default=True)
-@click.option('-p', '--personality')
-@click.option('--role', type=click.Choice(['system', 'user', 'assistant']), default='system')
+@click.option("--multiline/--singleline", default=True)
+@click.option("-p", "--personality")
+@click.option("--role", type=click.Choice(["system", "user", "assistant"]), default="system")
 @cli_search_options
 def add(personality, role, multiline, search_options):
     if any(search_options.values()):
         exchange = get_logged_exchange(**search_options)
-        messages = exchange['messages']
+        messages = exchange["messages"]
     else:
         messages = []
 
@@ -106,36 +135,44 @@ def list_tags():
     for tag in sorted(tags):
         click.echo(tag)
 
+
 @cli.command(help="Add tags to an exchange.", name="tag")
 @cli_search_options
-@click.argument('tags', nargs=-1)
+@click.argument("tags", nargs=-1)
 def add_tag(tags, search_options):
     exchange = get_logged_exchange(**search_options)
-    new_tags = [tag for tag in exchange.get('tags', []) if tag not in tags]
+    new_tags = [tag for tag in exchange.get("tags", []) if tag not in tags]
     new_tags.extend(tags)
 
-    write_log(messages=exchange['messages'], tags=new_tags)
+    write_log(messages=exchange["messages"], tags=new_tags)
+
 
 @cli.command(help="Remove tags from an exchange.")
 @cli_search_options
-@click.argument('tags', nargs=-1)
+@click.argument("tags", nargs=-1)
 def untag(tags, search_options):
     exchange = get_logged_exchange(**search_options)
-    new_tags = [t for t in exchange.get('tags', []) if t not in tags]
-    write_log(messages=exchange['messages'], tags=new_tags)
+    new_tags = [t for t in exchange.get("tags", []) if t not in tags]
+    write_log(messages=exchange["messages"], tags=new_tags)
+
 
 @cli.command(help="Current tag")
 @cli_search_options
 def show_tag(search_options):
     exchange = get_logged_exchange(**search_options)
-    tags = exchange.get('tags', [])
+    tags = exchange.get("tags", [])
 
     if tags:
         click.echo(tags[-1])
 
+
 @cli.command(help="Show a conversation.")
 @cli_search_options
-@click.option('-l/-s', '--long/--short', help="Show full conversation or just the most recent message.")
+@click.option(
+    "-l/-s",
+    "--long/--short",
+    help="Show full conversation or just the most recent message.",
+)
 def show(long, search_options):
     exchange = get_logged_exchange(**search_options)
     if long:
@@ -145,10 +182,10 @@ def show(long, search_options):
 
     for message in messages:
         prefix = ""
-        if message['role'] == 'user':
+        if message["role"] == "user":
             color = (186, 85, 211)
             prefix = ">> "
-        elif message['role'] == 'system':
+        elif message["role"] == "system":
             color = (100, 150, 200)
         else:
             color = None
@@ -157,23 +194,23 @@ def show(long, search_options):
 
 @cli.command(help="List all the questions we've asked")
 @cli_search_options
-@click.option('-l', '--limit', type=int, help="Limit number of results")
+@click.option("-l", "--limit", type=int, help="Limit number of results")
 def log(limit, search_options):
     for offset, exchange in reversed(list(itertools.islice(search_exchanges(**search_options), limit))):
-        messages = exchange['messages']
+        messages = exchange["messages"]
         if len(messages) > 1:
-            question = exchange['messages'][-2]['content']
+            question = exchange["messages"][-2]["content"]
         else:
-            question = exchange['messages'][-1]['content']
+            question = exchange["messages"][-1]["content"]
 
-        trimmed_message = question.split('\n', 1)[0][:80]
-        tags = click.style(f"{' '.join(exchange.get('tags', []))}", fg='green')
+        trimmed_message = question.split("\n", 1)[0][:80]
+        tags = click.style(f"{' '.join(exchange.get('tags', []))}", fg="green")
         click.echo(f"{click.style(f'{offset: 3d}:', fg='blue')} {trimmed_message} {tags}")
 
 
 # Add command to convert old chatlog format:
 @cli.command(help="Convert old chatlog format to new format.")
-@click.argument('filename', type=click.Path(exists=True))
+@click.argument("filename", type=click.Path(exists=True))
 def convert(filename, inplace=False):
     with open(filename, "r", encoding="utf-8") as fh:
         for line in fh:
@@ -201,7 +238,10 @@ def convert(filename, inplace=False):
 
             assert isinstance(messages, list), data
             assert isinstance(tags, list), data
-            assert isinstance(completion, dict) or completion is None, (completion, data)
+            assert isinstance(completion, dict) or completion is None, (
+                completion,
+                data,
+            )
             assert isinstance(usage, dict) or usage is None, (usage, data)
 
             converted_data = {
@@ -228,6 +268,7 @@ def conversation(request_messages, tags=None, stream=True, multiline=True, quick
         if quick:
             break
 
+
 def prompt(multiline=True):
     if os.isatty(0):
         try:
@@ -240,19 +281,14 @@ def prompt(multiline=True):
 
 
 def synchroneous_request(request_messages):
-    completion = openai.ChatCompletion.create(
-        model=ENGINE,
-        messages=request_messages)
+    completion = openai.ChatCompletion.create(model=ENGINE, messages=request_messages)
     click.echo(completion["choices"][0]["message"]["content"])
     return completion
 
+
 def stream_request(request_messages):
     completion = {}
-    for chunk in openai.ChatCompletion.create(
-            model=ENGINE,
-            messages = request_messages,
-            stream=True):
-
+    for chunk in openai.ChatCompletion.create(model=ENGINE, messages=request_messages, stream=True):
         if not completion:
             for key, value in chunk.items():
                 completion[key] = value
@@ -273,18 +309,21 @@ def stream_request(request_messages):
     click.echo()
     return completion
 
+
 def completion_usage(request_messages, completion):
-    if 'usage' in completion:
-        return completion['usage']
+    if "usage" in completion:
+        return completion["usage"]
 
     encoding = tiktoken.encoding_for_model(ENGINE)
-    request_text = ' '.join("role: " + x['role'] + " content: " + x['content'] + "\n" for x in request_messages)
+    request_text = " ".join("role: " + x["role"] + " content: " + x["content"] + "\n" for x in request_messages)
     request_tokens = len(encoding.encode(request_text))
     completion_tokens = len(encoding.encode(completion["choices"][0]["message"]["content"]))
     return {
-            "request_tokens": request_tokens,
-             "completion_tokens": completion_tokens,
-             "total_tokens": request_tokens + completion_tokens}
+        "request_tokens": request_tokens,
+        "completion_tokens": completion_tokens,
+        "total_tokens": request_tokens + completion_tokens,
+    }
+
 
 def answer(request_messages, stream=True, tags=None):
     if stream:
@@ -295,70 +334,85 @@ def answer(request_messages, stream=True, tags=None):
     response_message = completion["choices"][0]["message"]
 
     write_log(
-            messages = request_messages + [response_message],
-            completion = completion,
-            usage = completion_usage(request_messages, completion),
-            tags = tags)
+        messages=request_messages + [response_message],
+        completion=completion,
+        usage=completion_usage(request_messages, completion),
+        tags=tags,
+    )
 
     return response_message
 
+
 def cost(tokens):
     return tokens / 1000 * 0.002
+
 
 def write_log(messages, completion=None, usage=None, tags=None):
     assert isinstance(messages, list)
     assert isinstance(tags, list) or tags is None
     assert isinstance(completion, dict) or completion is None
     assert isinstance(usage, dict) or usage is None
-    with open(CHAT_LOG, "a", buffering=1, encoding='utf-8') as fh:
-        fh.write(json.dumps({
-            "messages": messages,
-            "completion": completion,
-            "usage": usage,
-            "tags": tags or []}) + "\n")
+    with open(CHAT_LOG, "a", buffering=1, encoding="utf-8") as fh:
+        fh.write(
+            json.dumps(
+                {
+                    "messages": messages,
+                    "completion": completion,
+                    "usage": usage,
+                    "tags": tags or [],
+                }
+            )
+            + "\n"
+        )
 
 
 def create_initial_log():
     for key, value in INITIAL_PERSONALITIES.items():
         write_log(messages=[{"role": "system", "content": value}], tags=["^" + key])
 
+
 def conversation_log():
     if not os.path.exists(CHAT_LOG):
         create_initial_log()
 
-    with open(CHAT_LOG, encoding='utf-8') as fh:
+    with open(CHAT_LOG, encoding="utf-8") as fh:
         return [json.loads(line) for line in fh]
+
 
 def search_exchanges(offset, search, tag):
     for idx, exchange in enumerate(reversed(conversation_log()), start=1):
         if offset and idx != offset:
             continue
 
-        if len(exchange['messages']) > 1:
-            question = exchange['messages'][-2]['content']
+        if len(exchange["messages"]) > 1:
+            question = exchange["messages"][-2]["content"]
         else:
-            question = exchange['messages'][-1]['content']
+            question = exchange["messages"][-1]["content"]
 
         if search and search not in question:
             continue
-        if tag and tag not in exchange.get('tags', []):
+        if tag and tag not in exchange.get("tags", []):
             continue
         yield idx, exchange
+
 
 def get_logged_exchange(offset, search=None, tag=None):
     return next(search_exchanges(offset, search, tag))[1]
 
+
 def get_tagged_exchange(tag):
     for exchange in reversed(conversation_log()):
-        if 'tags' in exchange and tag in exchange['tags']:
+        if "tags" in exchange and tag in exchange["tags"]:
             return exchange
     raise click.ClickException(f"No exchange with tag {tag} found.")
+
 
 @cli.command(help="Display number of tokens and token cost.", name="usage")
 def show_usage():
     tokens = sum(exchange["usage"]["total_tokens"] for exchange in conversation_log() if exchange["usage"])
-    click.echo(f'Tokens: {tokens}')
-    click.echo(f'Cost: ${cost(tokens):.2f}')
+    click.echo(f"Tokens: {tokens}")
+    click.echo(f"Cost: ${cost(tokens):.2f}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
--- tests/test_chatcli.py
+++ tests/test_chatcli.py
@@ -19,6 +19,7 @@ def fake_assistant(mocker):
             "choices": [{"message": {"role": "assistant", "content": messages[-1]["content"].upper()}}],
             "usage": {"total_tokens": 41},
         }
+
     mocker.patch("chatcli.chatcli.openai.ChatCompletion.create", advanced_ai)
 
 
@@ -59,13 +60,12 @@ def test_log(mocker):
         result = runner.invoke(
             cli, ["chat", "--quick", "-p", "concise"], input="What is your name?", catch_exceptions=False
         )
-        result = runner.invoke(
-            cli, ["chat", "--quick", "-c"], input="What is your quest?", catch_exceptions=False
-        )
+        result = runner.invoke(cli, ["chat", "--quick", "-c"], input="What is your quest?", catch_exceptions=False)
         result = runner.invoke(cli, ["log"], catch_exceptions=False)
         assert result.exit_code == 0
         assert "2: What is your name?" in result.output
         assert "1: What is your quest?" in result.output
+
 
 def test_chat_log_search():
     runner = CliRunner()
@@ -73,13 +73,12 @@ def test_chat_log_search():
         result = runner.invoke(
             cli, ["chat", "--quick", "-p", "concise"], input="What is your name?", catch_exceptions=False
         )
-        result = runner.invoke(
-            cli, ["chat", "--quick", "-c"], input="What is your quest?", catch_exceptions=False
-        )
+        result = runner.invoke(cli, ["chat", "--quick", "-c"], input="What is your quest?", catch_exceptions=False)
         result = runner.invoke(cli, ["log", "-s", "name"], catch_exceptions=False)
         assert result.exit_code == 0
         assert "2: What is your name?" in result.output
         assert "1: What is your quest?" not in result.output
+
 
 def test_usage():
     runner = CliRunner()
@@ -103,6 +102,7 @@ def test_tag_usage():
         assert "Tokens: 82" in result.output
         assert "Cost: $0.00" in result.output
 
+
 def test_untag_usage():
     runner = CliRunner()
     with runner.isolated_filesystem():
@@ -114,15 +114,15 @@ def test_untag_usage():
         assert "Tokens: 82" in result.output
         assert "Cost: $0.00" in result.output
 
+
 def test_chat_retry():
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(cli, ["chat"], input="What is your name?", catch_exceptions=False)
-        result = runner.invoke(
-            cli, ["chat", "--quick", "--retry"], catch_exceptions=False
-        )
+        result = runner.invoke(cli, ["chat", "--quick", "--retry"], catch_exceptions=False)
         assert result.exit_code == 0
         assert "WHAT IS YOUR NAME?" in result.output
+
 
 def test_tag():
     runner = CliRunner()
@@ -137,6 +137,7 @@ def test_tag():
         assert len(result.output.splitlines()) == 1
         assert "test_tag" in result.output
 
+
 def test_tags():
     runner = CliRunner()
     with runner.isolated_filesystem():
@@ -149,6 +150,7 @@ def test_tags():
         assert result.exit_code == 0
         assert "test_tag" in result.output
         assert "test_tag2" in result.output
+
 
 def test_tag_delete():
     runner = CliRunner()
@@ -163,6 +165,7 @@ def test_tag_delete():
         assert result.exit_code == 0
         assert "test_tag" not in result.output
 
+
 def test_show_tag():
     runner = CliRunner()
     with runner.isolated_filesystem():
@@ -174,6 +177,7 @@ def test_show_tag():
         assert result.exit_code == 0
         assert "test_tag" in result.output
 
+
 def test_current_tag_follows_conversation():
     runner = CliRunner()
     with runner.isolated_filesystem():
@@ -181,18 +185,24 @@ def test_current_tag_follows_conversation():
             cli, ["chat", "--quick", "-p", "concise"], input="What is your name?", catch_exceptions=False
         )
         result = runner.invoke(cli, ["tag", "test_tag"], catch_exceptions=False)
-        result = runner.invoke(cli, ["chat", "--continue", "--quick"], input="What is your quest?", catch_exceptions=False)
+        result = runner.invoke(
+            cli, ["chat", "--continue", "--quick"], input="What is your quest?", catch_exceptions=False
+        )
         result = runner.invoke(cli, ["show-tag"], catch_exceptions=False)
         assert result.exit_code == 0
         assert "test_tag" in result.output
 
+
 def test_add_personality():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(cli, ["add", "-p", "test_personality"], input="You are a test personality.", catch_exceptions=False)
+        result = runner.invoke(
+            cli, ["add", "-p", "test_personality"], input="You are a test personality.", catch_exceptions=False
+        )
         assert result.exit_code == 0
         result = runner.invoke(cli, ["log"], catch_exceptions=False)
         assert "^test_personality" in result.output
+
 
 def test_convert_is_noop():
     runner = CliRunner()
@@ -201,4 +211,6 @@ def test_convert_is_noop():
         result = runner.invoke(cli, ["tag", "test"], input="What is your name?", catch_exceptions=False)
         result = runner.invoke(cli, ["convert", "chatlog.log"], catch_exceptions=False)
         assert result.exit_code == 0
-        assert [json.loads(x) for x in result.output.splitlines()] == [json.loads(x) for x in open("chatlog.log", encoding="utf-8").read().splitlines()]
+        assert [json.loads(x) for x in result.output.splitlines()] == [
+            json.loads(x) for x in open("chatlog.log", encoding="utf-8").read().splitlines()
+        ]
