92a4dfc1d7e14341d81108a5549c518c1a4a3795
Adam Kelly <adam@cthulahoops.org>
2023-11-10 12:16:53 +0000

    Add a fixture that blocks http requests during tests.

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def _no_http_requests(monkeypatch):
+    def urlopen_mock(self, method, url, *_args, **_kwargs):
+        raise RuntimeError(f"The test was about to {method} {self.scheme}://{self.host}{url}")
+
+    monkeypatch.setattr("urllib3.connectionpool.HTTPConnectionPool.urlopen", urlopen_mock)
