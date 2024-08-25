141e72de3063a9443751bceebfc299c7e0d5f4a3
Adam Kelly <adam@cthulahoops.org>
2024-08-25 12:49:07 -0400

    Move fixtures into conftest.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,91 +4,9 @@ import json
 import os
 from pathlib import Path
 from unittest.mock import patch
-from click.testing import CliRunner
 import pytest
 from chatcli_gpt.cli import cli
-import chatcli_gpt.models
-
-
-@pytest.fixture(autouse=True)
-def _fake_assistant(mocker):
-    def ai(message, model):
-        if model == "name_is_alice":
-            return "My name is Alice."
-        if message.startswith("evaluate: "):
-            message = message.replace("evaluate: ", "")
-            return f"EVALUATE:\n```python\n{message}```"
-        return message.upper()
-
-    def streaming_ai(model, message):
-        yield {
-            "model": model,
-            "choices": [{"delta": {"role": "assistant"}, "index": 0}],
-        }
-        first = True
-        for word in ai(message, model).split(" "):
-            yield {
-                "choices": [
-                    {"delta": {"content": word if first else " " + word}, "index": 0}
-                ]
-            }
-            first = False
-
-    def advanced_ai(model, messages, *, stream=False, api_key=None, api_base=None):
-        if stream:
-            return (x for x in streaming_ai(model, messages[-1]["content"]))
-        return {
-            "choices": [
-                {
-                    "message": {
-                        "role": "assistant",
-                        "content": ai(messages[-1]["content"], model),
-                    },
-                },
-            ],
-            "usage": {"total_tokens": 41},
-        }
-
-    async def async_advanced_ai(*args, **kwargs):
-        async def agen(gen):
-            for x in gen:
-                yield x
-
-        return agen(advanced_ai(*args, **kwargs))
-
-    mocker.patch("openai.ChatCompletion.create", advanced_ai)
-    mocker.patch("openai.ChatCompletion.acreate", async_advanced_ai)
-    mocker.patch(
-        "chatcli_gpt.conversation.completion_usage",
-        return_value={"prompt_tokens": 11, "completion_tokens": 10, "total_tokens": 41},
-    )
-
-
-@pytest.fixture()
-def chatcli(mocker):
-    mocker.patch("chatcli_gpt.models.MODEL_CACHE", Path(".chatcli-models.json"))
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        chatcli_gpt.models.MODEL_CACHE.open("w").write(
-            json.dumps(
-                [
-                    {
-                        "id": "name_is_alice",
-                        "pricing": {"prompt": 0.002, "completion": 0.002},
-                    }
-                ]
-            )
-        )
-
-        def chatcli(*args, catch_exceptions=False, expected_exit_code=0, **kwargs):
-            result = runner.invoke(
-                cli, *args, catch_exceptions=catch_exceptions, **kwargs
-            )
-            assert result.exit_code == expected_exit_code
-            return result
-
-        chatcli("init")
-        yield chatcli
+from click.testing import CliRunner
 
 
 def test_chat_default(chatcli):
